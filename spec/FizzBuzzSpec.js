4d3fdaee2f28eaf120928bc852dadb7a0cbcb958
NickMountjoy <nick.mountjoy98@gmail.com>
2016-03-29 14:49:07 +0100

    FizzBuzz Complete

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -7,11 +7,34 @@ describe("FizzBuzz", function(){
   describe('multiples of three', function(){
 
     it("should return fizz if number divisible by three", function(){
-
       expect(fizzBuzz.output(3)).toEqual('fizz');
 
     });
   });
+
+  describe('multiples of five', function(){
+
+    it("should return buzz if number divisible by five", function(){
+      expect(fizzBuzz.output(5)).toEqual('buzz');
+
+  });
+});
+
+describe('multiples of three and five', function(){
+
+  it("should return FizzBuzzbuzz if number divisible by both three and five", function(){
+    expect(fizzBuzz.output(15)).toEqual('fizzbuzz');
+
+});
+});
+
+describe('returns number', function() {
+  it("should return number if not divisible by three of five", function(){
+    expect(fizzBuzz.output(13)).toEqual(13);
+  });
+});
+
+
   /*
 
   it("should return error if number less than 1 or greater than 100", function({
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -4,7 +4,13 @@ function FizzBuzz(){
 
 FizzBuzz.prototype.output = function(n){
   console.log(n);
-  if (n % 3 == 0){
+  if ( n % 15 == 0){
+    return 'fizzbuzz';
+  } else if (n % 3 == 0){
     return 'fizz';
-  };
+  } else if (n % 5 == 0) {
+    return 'buzz';
+  } else {
+    return n;
+    }
 };
